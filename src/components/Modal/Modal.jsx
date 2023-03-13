150ef3f90fbc16f280a59b9037602c8ba6de8f5e
dilyara <di.salakhovaa@mail.ru>
2023-03-13 10:20:32 +0300

    ..

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -53,18 +53,6 @@ function Modal() {
                                 <a href="#" className="modal__delete">Удалить</a>
                             </div>
                         </div>
-
-                        <div className="modal__item">
-                            <div className="modal__image">
-                                <img src={image} alt=""/>
-                            </div>
-
-                            <div className="modal__information">
-                                <h3 className="modal__name">dhbguvbg</h3>
-                                <p className="modal__price">2 260 ₽</p>
-                                <a href="#" className="modal__delete">Удалить</a>
-                            </div>
-                        </div>
                     </div>
                 </div>
 
